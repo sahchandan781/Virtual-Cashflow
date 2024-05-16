1af8bc0d7c5e2f6774217e8d6f203a48129cefa7
Chandan kumar Sah <93442672+sahchandan781@users.noreply.github.com>
2024-05-16 16:16:35 +0530

    Add files via upload

---
+++ Bank Management System/src/bank/management/system/Conn.java
@@ -0,0 +1,20 @@
+
+package bank.management.system;
+import java.sql.*;
+
+public class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagementsystem","root","9803288022C@abc");    
+            s =c.createStatement(); 
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
